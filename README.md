76badfc01fae317846d3637805253b198dcc69c0
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-09-24 15:33:11 +0800

    project final

--- README.md
+++ README.md
@@ -4,22 +4,29 @@ Battleship Pre-project Planning
 
 Game Outline
 
--2 players
--Each player receives a square grid(width tbd) 
--Each player also receives a set of ships(ships amount tbd)
--Each of the ships will be assigned coordinates that corresponds to the player’s grid
--Each player take turns to guess 1 ship coordinate
--Each guess is unique and cannot be repeated 
--First player to successfully guess all of the other players’ ship coordinates win 
+1. 2 players
+2. Each player receives a square grid(width tbd) 
+3. Each player also receives a set of ships(ships amount tbd)
+4. Each of the ships will be assigned coordinates that corresponds to the player’s grid
+5. Each player take turns to guess 1 ship coordinate
+6. Each guess is unique and cannot be repeated 
+7. First player to successfully guess all of the other players’ ship coordinates win 
 
 
 Minimum Viable Product
 
--5x5 grid
--Play against human opponent 
--3 ships
--Ship length = 1
--Guessing ship coordinate mechanism works
--What happens when guess correctly, what happens when guess incorrectly
--End turn function
--Win state
+1. 5x5 grid
+2. Play against human opponent 
+3. 3 ships
+4. Ship length = 1
+5. Guessing ship coordinate mechanism works
+6. What happens when guess correctly, what happens when guess incorrectly
+7. End turn function
+8. Win state
+
+For Future Development
+
+1. Make online multi-player viable
+2. Multiple ship classes with varying lengths
+3. Variable grid size
+4. Make the ships move at the end of every turn
--- app.js
+++ app.js
@@ -70,220 +70,239 @@ window.addEventListener("DOMContentLoaded", (event) => {
       userCell.addEventListener("click", changePlayerColorOnClick, true);
     }
   }
-  function changePlayerColorOnClick(event) {
-    event.target.style.backgroundColor = "red";
-    event.target.style.zIndex = "(1)";
+
+
+createUserBoard(userGrid, userArray, gridWidth);
+
+console.log("user board created");
+
+// ============================================================================================
+
+//* CPU Board
+function createCpuBoard(cpuGrid, square, width) {
+  for (let i = 0; i < width * width; i++) {
+    const cpuCell = document.createElement("div");
+    cpuCell.setAttribute("class", "emptyCpuCell");
+    cpuCell.setAttribute("id", i + totalGridSize);
+    cpuGrid.appendChild(cpuCell);
+    square.push(cpuCell);
+    cpuCell.addEventListener("click", changeCpuColorOnClick, true);
+    console.log(cpuCell);
   }
+}
 
-  createUserBoard(userGrid, userArray, gridWidth);
+createCpuBoard(cpuGrid, cpuArray, gridWidth);
 
-  console.log("user board created");
+console.log("cpu board created");
 
-  // ============================================================================================
+//* ============================================================================================
+//* ============================================================================================
+//* ============================================================================================
 
-  //* CPU Board
-  function createCpuBoard(cpuGrid, square, width) {
-    for (let i = 0; i < width * width; i++) {
-      const cpuCell = document.createElement("div");
-      cpuCell.setAttribute("class", "emptyCpuCell");
-      cpuCell.setAttribute("id", i + totalGridSize);
-      cpuGrid.appendChild(cpuCell);
-      square.push(cpuCell);
-      cpuCell.addEventListener("click", changeCpuColorOnClick, true);
-      console.log(cpuCell);
-    }
+// Selects random starting square for ships and render ships on grid
+
+// Takes the random number from below, creates and appends a div to a div with that number as and id
+// GENERATE SHIP 1
+function shipPlayerImg1(placeHolder1) {
+  const grabCell1 = document.getElementById(`${placeHolder1.id}`);
+  let createCell1 = document.createElement("div");
+  
+  createCell1.setAttribute("id", "Ship1");
+  createCell1.setAttribute("class", "shipActual");
+  grabCell1.appendChild(createCell1);
+  
+  shipArray.push(createCell1);
+}
+// Select random number from 0-24
+const element1 = userArray[Math.floor(Math.random() * userArray.length)];
+
+function createPlayerShip1() {
+  shipPlayerImg1(element1);
+}
+console.log(`ship1 ok ` + `${element1.id}`);
+// ============================================================================================
+
+// GENERATE SHIP 2
+function shipPlayerImg2(placeHolder2) {
+  let grabCell2 = document.getElementById(`${placeHolder2.id}`);
+  let createCell2 = document.createElement("div");
+  
+  createCell2.setAttribute("id", "Ship2");
+  createCell2.setAttribute("class", "shipActual");
+  grabCell2.appendChild(createCell2);
+  
+  shipArray.push(createCell2);
+}
+
+let element2 = userArray[Math.floor(Math.random() * userArray.length)];
+
+function createPlayerShip2() {
+  while (element1.id === element2.id) {
+    element2 = userArray[Math.floor(Math.random() * userArray.length)];
   }
+  shipPlayerImg2(element2);
+}
+console.log(`ship2 ok ` + `${element2.id}`);
+// ============================================================================================
 
-  createCpuBoard(cpuGrid, cpuArray, gridWidth);
+// GENERATE SHIP 3
+function shipPlayerImg3(placeHolder3) {
+  let grabCell3 = document.getElementById(`${placeHolder3.id}`);
+  let createCell3 = document.createElement("div");
+  
+  createCell3.setAttribute("id", "Ship3");
+  createCell3.setAttribute("class", "shipActual");
+  grabCell3.appendChild(createCell3);
+  
+  shipArray.push(createCell3);
+}
 
-  console.log("cpu board created");
+let element3 = userArray[Math.floor(Math.random() * userArray.length)];
 
-  //* ============================================================================================
-  //* ============================================================================================
-  //* ============================================================================================
-
-  // Selects random starting square for ships and render ships on grid
-
-  // Takes the random number from below, creates and appends a div to a div with that number as and id
-  // GENERATE SHIP 1
-  function shipPlayerImg1(placeHolder1) {
-    const grabCell1 = document.getElementById(`${placeHolder1.id}`);
-    let createCell1 = document.createElement("div");
-
-    createCell1.setAttribute("id", "Ship1");
-    createCell1.setAttribute("class", "shipActual");
-    grabCell1.appendChild(createCell1);
-
-    shipArray.push(createCell1);
+function createPlayerShip3() {
+  while (element3.id === element1.id || element3.id === element2.id) {
+    element3 = userArray[Math.floor(Math.random() * userArray.length)];
   }
-  // Select random number from 0-24
-  const element1 = userArray[Math.floor(Math.random() * userArray.length)];
+  shipPlayerImg3(element3);
+}
+console.log(`ship3 ok ` + `${element3.id}`);
+console.log("PLAYER ships generated");
 
-  function createPlayerShip1() {
-    shipPlayerImg1(element1);
+//* ============================================================================================
+//* ============================================================================================
+//* ============================================================================================
+
+// GENERATE SHIP 4
+function shipCpuImg1(placeHolder4) {
+  const grabCell1 = document.getElementById(`${placeHolder4.id}`);
+  let createCell1 = document.createElement("div");
+  
+  createCell1.setAttribute("id", "Ship4");
+  createCell1.setAttribute("class", "shipActual");
+  grabCell1.appendChild(createCell1);
+  
+  shipArray.push(createCell1);
+}
+
+// Select random number from 25-49
+const element4 = cpuArray[Math.floor(Math.random() * cpuArray.length)];
+
+function createCpuShip1() {
+  shipCpuImg1(element4);
+}
+console.log(`ship4 ok ` + `${element4.id}`);
+// ============================================================================================
+
+// GENERATE SHIP 5
+function shipCpuImg2(placeHolder5) {
+  let grabCell2 = document.getElementById(`${placeHolder5.id}`);
+  let createCell2 = document.createElement("div");
+  
+  createCell2.setAttribute("id", "Ship5");
+  createCell2.setAttribute("class", "shipActual");
+  grabCell2.appendChild(createCell2);
+  
+  shipArray.push(createCell2);
+}
+
+let element5 = cpuArray[Math.floor(Math.random() * cpuArray.length)];
+
+function createCpuShip2() {
+  while (element5.id === element4.id) {
+    element5 = cpuArray[Math.floor(Math.random() * cpuArray.length)];
   }
-  console.log(`ship1 ok ` + `${element1.id}`);
-  // ============================================================================================
+  shipCpuImg2(element5);
+}
+console.log(`ship5 ok ` + `${element5.id}`);
+// ============================================================================================
 
-  // GENERATE SHIP 2
-  function shipPlayerImg2(placeHolder2) {
-    let grabCell2 = document.getElementById(`${placeHolder2.id}`);
-    let createCell2 = document.createElement("div");
+// GENERATE SHIP 6
+function shipCpuImg3(placeHolder6) {
+  let grabCell3 = document.getElementById(`${placeHolder6.id}`);
+  let createCell3 = document.createElement("div");
+  
+  createCell3.setAttribute("id", "Ship6");
+  createCell3.setAttribute("class", "shipActual");
+  grabCell3.appendChild(createCell3);
+  
+  shipArray.push(createCell3);
+}
 
-    createCell2.setAttribute("id", "Ship2");
-    createCell2.setAttribute("class", "shipActual");
-    grabCell2.appendChild(createCell2);
+let element6 = cpuArray[Math.floor(Math.random() * cpuArray.length)];
 
-    shipArray.push(createCell2);
+function createCpuShip3() {
+  while (element6.id === element5.id || element6.id === element4.id) {
+    element6 = cpuArray[Math.floor(Math.random() * cpuArray.length)];
   }
+  shipCpuImg3(element6);
+}
 
-  let element2 = userArray[Math.floor(Math.random() * userArray.length)];
+console.log(`ship6 ok ` + `${element6.id}`);
+console.log("CPU ships generated");
 
-  function createPlayerShip2() {
-    while (element1.id === element2.id) {
-      element2 = userArray[Math.floor(Math.random() * userArray.length)];
-    }
-    shipPlayerImg2(element2);
-  }
-  console.log(`ship2 ok ` + `${element2.id}`);
-  // ============================================================================================
+//* ============================================================================================
+//* ============================================================================================
+//* ============================================================================================
 
-  // GENERATE SHIP 3
-  function shipPlayerImg3(placeHolder3) {
-    let grabCell3 = document.getElementById(`${placeHolder3.id}`);
-    let createCell3 = document.createElement("div");
+// START GAME
 
-    createCell3.setAttribute("id", "Ship3");
-    createCell3.setAttribute("class", "shipActual");
-    grabCell3.appendChild(createCell3);
+//? All event listeners are here
 
-    shipArray.push(createCell3);
-  }
+// Initialize ship funtions
+startButton.addEventListener("click", () => {
+  createPlayerShip1();
+  createPlayerShip2();
+  createPlayerShip3();
+  createCpuShip1();
+  createCpuShip2();
+  createCpuShip3();
+  turnDisplay.innerHTML = `${player1Text}`;
+  console.log(shipArray);
+  
+  // SHIP specific events listeners
+  document
+  .getElementById("Ship1")
+  .addEventListener("click", changeShipColorOnClick, true);
+  document
+  .getElementById("Ship2")
+  .addEventListener("click", changeShipColorOnClick, true);
+  document
+  .getElementById("Ship3")
+  .addEventListener("click", changeShipColorOnClick, true);
+  document
+  .getElementById("Ship4")
+  .addEventListener("click", changeShipColorOnClick, true);
+  document
+  .getElementById("Ship5")
+  .addEventListener("click", changeShipColorOnClick, true);
+  document
+  .getElementById("Ship6")
+  .addEventListener("click", changeShipColorOnClick, true);
+});
 
-  let element3 = userArray[Math.floor(Math.random() * userArray.length)];
+// ============================================================================================
 
-  function createPlayerShip3() {
-    while (element3.id === element1.id || element3.id === element2.id) {
-      element3 = userArray[Math.floor(Math.random() * userArray.length)];
-    }
-    shipPlayerImg3(element3);
-  }
-  console.log(`ship3 ok ` + `${element3.id}`);
-  console.log("PLAYER ships generated");
+// Add change color on click to created element
 
-  //* ============================================================================================
-  //* ============================================================================================
-  //* ============================================================================================
+function changeCpuColorOnClick(event) {
+  event.target.style.backgroundColor = "red";
+}
 
-  // GENERATE SHIP 4
-  function shipCpuImg1(placeHolder4) {
-    const grabCell1 = document.getElementById(`${placeHolder4.id}`);
-    let createCell1 = document.createElement("div");
+function changeShipColorOnClick(event) {
+  event.target.style.backgroundColor = "green";
+  event.target.style.zIndex = "5";
+  event.target.style.opacity = "1";
+}
 
-    createCell1.setAttribute("id", "Ship4");
-    createCell1.setAttribute("class", "shipActual");
-    grabCell1.appendChild(createCell1);
+function changePlayerColorOnClick(event) {
+  event.target.style.backgroundColor = "red";
+  event.target.style.zIndex = "(1)";
+}
+  // Event Listeners for Main Board to identify cell
 
-    shipArray.push(createCell1);
-  }
+  document.getElementById("grid1").addEventListener("click", playerTurn);
 
-  // Select random number from 25-49
-  const element4 = cpuArray[Math.floor(Math.random() * cpuArray.length)];
-
-  function createCpuShip1() {
-    shipCpuImg1(element4);
-  }
-  console.log(`ship4 ok ` + `${element4.id}`);
-  // ============================================================================================
-
-  // GENERATE SHIP 5
-  function shipCpuImg2(placeHolder5) {
-    let grabCell2 = document.getElementById(`${placeHolder5.id}`);
-    let createCell2 = document.createElement("div");
-
-    createCell2.setAttribute("id", "Ship5");
-    createCell2.setAttribute("class", "shipActual");
-    grabCell2.appendChild(createCell2);
-
-    shipArray.push(createCell2);
-  }
-
-  let element5 = cpuArray[Math.floor(Math.random() * cpuArray.length)];
-
-  function createCpuShip2() {
-    while (element5.id === element4.id) {
-      element5 = cpuArray[Math.floor(Math.random() * cpuArray.length)];
-    }
-    shipCpuImg2(element5);
-  }
-  console.log(`ship5 ok ` + `${element5.id}`);
-  // ============================================================================================
-
-  // GENERATE SHIP 6
-  function shipCpuImg3(placeHolder6) {
-    let grabCell3 = document.getElementById(`${placeHolder6.id}`);
-    let createCell3 = document.createElement("div");
-
-    createCell3.setAttribute("id", "Ship6");
-    createCell3.setAttribute("class", "shipActual");
-    grabCell3.appendChild(createCell3);
-
-    shipArray.push(createCell3);
-  }
-
-  let element6 = cpuArray[Math.floor(Math.random() * cpuArray.length)];
-
-  function createCpuShip3() {
-    while (element6.id === element5.id || element6.id === element4.id) {
-      element6 = cpuArray[Math.floor(Math.random() * cpuArray.length)];
-    }
-    shipCpuImg3(element6);
-  }
-
-  console.log(`ship6 ok ` + `${element6.id}`);
-  console.log("CPU ships generated");
-
-  //* ============================================================================================
-  //* ============================================================================================
-  //* ============================================================================================
-
-  // START GAME
-
-  startButton.addEventListener("click", () => {
-    createPlayerShip1();
-    createPlayerShip2();
-    createPlayerShip3();
-    createCpuShip1();
-    createCpuShip2();
-    createCpuShip3();
-    turnDisplay.innerHTML = `${player1Text}`;
-    console.log(shipArray);
-
-    function changeShipColorOnClick(event) {
-      event.target.style.backgroundColor = "green";
-      event.target.style.zIndex = "5";
-      event.target.style.opacity = "1";
-    }
-
-    document
-      .getElementById("Ship1")
-      .addEventListener("click", changeShipColorOnClick, true);
-    document
-      .getElementById("Ship2")
-      .addEventListener("click", changeShipColorOnClick, true);
-    document
-      .getElementById("Ship3")
-      .addEventListener("click", changeShipColorOnClick, true);
-    document
-      .getElementById("Ship4")
-      .addEventListener("click", changeShipColorOnClick, true);
-    document
-      .getElementById("Ship5")
-      .addEventListener("click", changeShipColorOnClick, true);
-    document
-      .getElementById("Ship6")
-      .addEventListener("click", changeShipColorOnClick, true);
-  });
+  document.getElementById("grid0").addEventListener("click", cpuTurn);
 
   //* ============================================================================================
   //* ============================================================================================
@@ -292,9 +311,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
   // Game Logic
 
   // Logic for Player 1 Turn
-  document.getElementById("grid1").addEventListener("click", playerTurn);
-
-  document.getElementById("grid0").addEventListener("click", cpuTurn);
 
   function playerTurn(event) {
     if (currentPlayer === "player1") {
@@ -326,7 +342,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
   function cpuTurn(event) {
     if (currentPlayer === "player2") {
       turnDisplay.innerHTML = `${player2Text}`;
-    };
+    }
     console.log(event.target.id);
     if (
       event.target.id === "Ship1" ||
@@ -349,12 +365,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     }
   }
 
-  //? Color Changes
-
-  // Add change color on click to created element
-  function changeCpuColorOnClick(event) {
-    event.target.style.backgroundColor = "red";
-  }
   //* ============================================================================================
   //* ============================================================================================
   //* ============================================================================================
